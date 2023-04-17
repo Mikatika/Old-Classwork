ccac37131ed49f6687fcdddc76a0eca0a494d9ef
Mikatika <116248110+Mikatika@users.noreply.github.com>
2023-04-16 22:52:12 -0700

    Add files via upload

---
+++ CS61A Work/Labs/lab00/lab00.py
@@ -0,0 +1,8 @@
+def twenty_twenty():
+    """Come up with the most creative expression that evaluates to 2020,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty()
+    2020
+    """
+    return 1*2*3*3*4*5*6-1*2*3*4*5-1*2*3*4+1*2*3-1*2
